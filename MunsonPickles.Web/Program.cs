60723fb9c02ab4f4db9360217c065fc438460afc
Ashish Khanal <affableashk@gmail.com>
2023-09-29 15:59:33 -0400

    Comment III

--- MunsonPickles.Web/Program.cs
+++ MunsonPickles.Web/Program.cs
@@ -74,7 +74,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 // Add the Microsoft Identity Web cookie policy
-//app.UseCookiePolicy();
+//app.UseCookiePolicy();Some comment III
 
 // Add the ASP.NET Core authentication service
 //app.UseAuthentication();
